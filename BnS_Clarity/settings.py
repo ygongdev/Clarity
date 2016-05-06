03639488125f84b27babef2212ce545c6bf3070c
ygong10 <yichenggong567@gmail.com>
2016-05-05 23:25:55 -0500

    serving static files

--- BnS_Clarity/settings.py
+++ BnS_Clarity/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '*#@41#w3g=p5_i&us2cl763vc6!+6vi_)x^--zdx71_6cpn+)-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*",]
 
