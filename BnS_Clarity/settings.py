d24d6dbee79dac852364904e097f4b9077bc6b56
ygong10 <yichenggong567@gmail.com>
2016-05-05 21:09:59 -0500

    static

--- BnS_Clarity/settings.py
+++ BnS_Clarity/settings.py
@@ -122,12 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = ''
+STATIC_ROOT = 'static'
 
-if DEBUG:
-    EMAIL_HOST = 'localhost'
-    EMAIL_PORT = 1025
-    EMAIL_HOST_USER = ''
-    EMAIL_HOST_PASSWORD = ''
-    EMAIL_USE_TLS = False
-    DEFAULT_FROM_EMAIL = 'testing@example.com'
+
+
